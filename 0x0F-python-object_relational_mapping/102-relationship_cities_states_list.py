b3352f2a7c0df5349de4415d5da95cac24e19f68
Ahmad Muhammad <kowaslaba2015@gmail.com>
2023-03-19 21:42:41 -0700

    city relationship

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This script defines a City class
+to work with MySQLAlchemy ORM.
+"""
+
+from relationship_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+
